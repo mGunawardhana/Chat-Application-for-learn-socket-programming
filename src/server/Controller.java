818cbaff65cfe7665b5427aedd896875418794d1
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-08-01 22:48:00 +0530

    setting up server side ui

---
+++ src/server/Controller.java
@@ -0,0 +1,18 @@
+package server;
+/*
+ * Developed by - mGunawardhana
+ * Contact email - mrgunawardhana27368@gmail.com
+ * what's app - 071 - 9043372
+ */
+
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class Controller {
+    public JFXButton button_send;
+    public TextField tf_message;
+    public ScrollPane sp_main;
+    public VBox vbox_messages;
+}
---
+++ src/server/Main.java
@@ -0,0 +1,30 @@
+package server;
+/*
+ * Developed by - mGunawardhana
+ * Contact email - mrgunawardhana27368@gmail.com
+ * what's app - 071 - 9043372
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("server" +
+                "");
+        primaryStage.setScene(new Scene(root,600,600));
+        primaryStage.show();
+    }
+}
