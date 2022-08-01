a2e1fa2e66e4904962b4f131ec3f5fdcf48a8b5f
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-08-01 23:29:27 +0530

    implementing the initialze method in chat

--- src/server/Controller.java
+++ src/server/Controller.java
@@ -6,13 +6,90 @@ package server;
  */
 
 import com.jfoenix.controls.JFXButton;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
-public class Controller {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
     public JFXButton button_send;
+
+    @FXML
     public TextField tf_message;
+
+    @FXML
     public ScrollPane sp_main;
+
+    @FXML
     public VBox vbox_messages;
+    private Server server;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            server = new Server(new ServerSocket(8000));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error creating server");
+        }
+
+        vbox_messages.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                sp_main.setVvalue((Double) newValue);
+            }
+        });
+
+        server.recieveMessageFromClient(vbox_messages);
+
+        button_send.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String messageToSend = tf_message.getText();
+                if (!messageToSend.isEmpty()) {
+                    HBox hBox = new HBox();
+                    hBox.setAlignment(Pos.CENTER_RIGHT);
+                    hBox.setPadding(new Insets(5, 5, 5, 10));
+
+                    Text text = new Text(messageToSend);
+                    TextFlow textFlow = new TextFlow(text);
+
+                    textFlow.setStyle("-fx-color: rgb(239,242,255);" +
+                            "-fx-background-color: rgb(15,125,242);" +
+                            " -fx-background-radius: 20px");
+
+
+                    textFlow.setPadding(new Insets(5,10,5,10));
+                    text.setFill(Color.color(0.934,0.945,0.996));
+
+                    hBox.getChildren().add(textFlow);
+                    vbox_messages.getChildren().add(hBox);
+
+                    server.sendMessageToClient(messageToSend);
+                    tf_message.clear();
+                }
+            }
+        });
+
+
+
+    }
 }
